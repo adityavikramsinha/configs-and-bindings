6156ab2b60b0a18b23ee09ad952980da342a9c73
unknown <adityavikramsinha19@gmail.com>
2025-03-29 16:26:14 +0530

    lualines config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,4 +1,57 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        cust_horizon = require('lualine.themes.horizon')
+
+        -- To make the in background transparent we have to make it nil, no other option
+        cust_horizon.insert.c.bg = nil
+        cust_horizon.normal.c.bg = nil
+        cust_horizon.command.c.bg = nil
+        cust_horizon.inactive.c.bg = nil
+        cust_horizon.replace.c.bg = nil
+        cust_horizon.visual.c.bg = nil
+        require('lualine').setup {
+
+            options = {
+                icons_enabled = true,
+                theme = 'horizon ',
+                component_separators = { left = '', right = ''},
+                section_separators = { left = '', right = ''},
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 100,
+                    tabline = 100,
+                    winbar = 100,
+                }
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
+    end
 }
